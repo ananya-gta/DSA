8f485dbf028c4ae35a9d83193e016abb5bb6bab4
Ananya Gupta <anannya.gta@gmail.com>
2026-08-05 12:50:47 +0530

    Time: 8 ms (27.61%), Space: 52.2 MB (5.34%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,27 @@
 class Solution {
+    Integer[][] dp;
     public boolean stoneGame(int[] piles) {
-        return true;
+        int sum = 0;
+        int n = piles.length;
+        dp = new Integer[n][n];
+        for (int pile : piles) sum += pile;
+        int aliceScore = findAliceScore(0, piles.length - 1, piles);
+        return aliceScore > sum/2;
+    }
+
+    public int findAliceScore(int i, int j, int[] piles) {
+        if (i > j) return 0;
+        if (i == j) return piles[i];
+        if (dp[i][j] != null)
+            return dp[i][j];
+        int AliceTakesFirst = piles[i] + Math.min(
+            findAliceScore(i + 2, j, piles),
+            findAliceScore(i + 1, j - 1, piles)
+        );
+        int AliceTakesLast = piles[j] + Math.min(
+            findAliceScore(i + 1, j - 1, piles),
+            findAliceScore(i, j - 2, piles)
+        );
+        return dp[i][j] = Math.max(AliceTakesFirst, AliceTakesLast);
     }
 }
